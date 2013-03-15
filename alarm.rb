75deec2ce6e1c32f3e36bdda8ea4e2804d788f2e
coderhs <mailme@hsps.in>
2013-03-15 20:35:41 +0530

    Gave a proper name to program

---
+++ alarm.rb
@@ -0,0 +1,24 @@
+class Alarm
+  def initialize(alarm_time, music = '/mount/data2/Songs/11.Mazhathullikal.mp3')
+    @alarm_time = alarm_time
+    @music = music
+    @run = true
+  end
+
+  def its_time
+    system("vlc #{@music} --volume 512")
+  end
+
+  def check_time
+    while @run
+      if Time.now >= @alarm_time
+        its_time
+        @run = false
+      else
+        sleep(@alarm_time - Time.now)
+      end
+    end
+  end
+end
+@alarm = Alarm.new Time.new 2013, 03, 15, 6
+@alarm.check_time
