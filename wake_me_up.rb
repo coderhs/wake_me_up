f59d86250470d815577bf9033063fbb82ff4ac79
coderhs <mailme@hsps.in>
2013-03-15 13:31:17 +0530

    Modified code to match Ruby Style Guide

--- wake_me_up.rb
+++ wake_me_up.rb
@@ -1,10 +1,10 @@
 class WakeMeUp
-  def initialize(alarm_time,music = "/mount/data2/Songs/11.Mazhathullikal.mp3")
+  def initialize(alarm_time, music = '/mount/data2/Songs/11.Mazhathullikal.mp3')
     @alarm_time = alarm_time
     @music = music
     @run = true
   end
- 
+
   def its_time
     system("vlc #{@music} --volume 512")
   end
@@ -13,12 +13,12 @@ class WakeMeUp
     while @run
       if Time.now >= @alarm_time
         its_time
-	@run = false
+        @run = false
       else
-        sleep(@alarm_time-Time.now)
+        sleep(@alarm_time - Time.now)
       end
     end
   end
 end
-@wake_me_up = WakeMeUp.new Time.new 2013,03,15,6
+@wake_me_up = WakeMeUp.new Time.new 2013, 03, 15, 6
 @wake_me_up.check_time
